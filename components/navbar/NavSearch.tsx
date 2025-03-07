99877a0dd33325074743b30c210257fc4930c6af
Yatharth Shah <yatharhshah00@gmail.com>
2025-03-07 10:23:01 -0500

    Fix NavSearch component

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,31 +1,74 @@
+// "use client";
+
+// import { Input } from "../ui/input";
+// import { useSearchParams, useRouter } from "next/navigation";
+// import { useDebouncedCallback } from "use-debounce";
+// import { useState, useEffect } from "react";
+
+// function NavSearch() {
+//   const searchParams = useSearchParams();
+
+//   const {replace} = useRouter();
+//   const [search, setSearch] = useState(searchParams.get("search")?.toString || "");
+//   const handleSearch = useDebouncedCallback((value: string) => {
+//     const params = new URLSearchParams(searchParams);
+//     if (value) {
+//       params.set("search", value);
+//     }
+//     else {
+//       params.delete("search");
+//     }
+//     replace(`$/?${params.toString()}`);
+//   },500);
+
+//  useEffect(() => {
+//   if(!searchParams.get("search")) {
+//     setSearch("");
+//   }
+//  }, [searchParams.get("search")]);
+//   return (
+//     <Input
+//       type="text"
+//       placeholder="find a property..."
+//       className="max-w-xs dark:bg-muted"
+//       onChange={(e) => {
+//         setSearch(e.target.value);
+//         handleSearch(e.target.value);
+//       }}
+//       value={search}
+//     />
+//   );
+// }
+
+// export default NavSearch;
 "use client";
 
 import { Input } from "../ui/input";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 import { useState, useEffect } from "react";
 
 function NavSearch() {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const {replace} = useRouter();
-  const [search, setSearch] = useState(searchParams.get("search")?.toString || "");
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(searchParams.get("search") || "");
+
   const handleSearch = useDebouncedCallback((value: string) => {
     const params = new URLSearchParams(searchParams);
     if (value) {
       params.set("search", value);
-    }
-    else {
+    } else {
       params.delete("search");
     }
-    replace(`${pathname}?${params.toString()}`);
-  },500);
+    replace(`/?${params.toString()}`);
+  }, 500);
 
- useEffect(() => {
-  if(!searchParams.get("search")) {
-    setSearch("");
-  }
- }, [searchParams.get("search")]);
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams]);
+
   return (
     <Input
       type="text"
